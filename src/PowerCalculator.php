f9477c37830c40927b585964437e6176b1a0e79d
Manish Kumar <manish.br.kumar@oracle.com>
2021-07-21 19:00:58 +0530

    Renaming to php fixer

--- src/PowerCalculator.php
+++ src/PowerCalculator.php
@@ -3,23 +3,24 @@
 
 namespace Bronto\StarterPackage;
 
-
 class PowerCalculator
 {
     private float $watt;
     private float $hours;
 
-    public function __construct(float $watt, float $hours){
-
+    public function __construct(float $watt, float $hours)
+    {
         $this->watt = $watt;
         $this->hours = $hours;
     }
 
-    public static function fromWattAndHour(float $watt, float $hours): self {
+    public static function fromWattAndHour(float $watt, float $hours): self
+    {
         return new static($watt, $hours);
     }
 
-    public function intoKWH():float {
+    public function intoKWH():float
+    {
         return ($this->watt * $this->hours) / 1000;
     }
 }
--- tests/PowerCalculatorTest.php
+++ tests/PowerCalculatorTest.php
@@ -3,15 +3,15 @@
 
 namespace Bronto\StarterPackage\Tests;
 
-
 use Bronto\StarterPackage\PowerCalculator;
 use PHPUnit\Framework\TestCase;
 
 class PowerCalculatorTest extends TestCase
 {
-      /** @test **/
-      public function convert_watt_hours_kwh() {
-          $power = PowerCalculator::fromWattAndHour(100, 10);
-          $this->assertEquals(1, $power->intoKWH());
-      }
+    /** @test **/
+    public function convert_watt_hours_kwh()
+    {
+        $power = PowerCalculator::fromWattAndHour(100, 10);
+        $this->assertEquals(1, $power->intoKWH());
+    }
 }
